b18a090c8d8ce5c24ce3bf67b3e0b6d079353a25
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2021-09-30 23:19:37 +0530

    Updated design file for registration

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,13 +1,16 @@
 $(function () {
     
+    $('.toast').toast({ delay: 5000 });
+
     validateRegistration = function () {
+        var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
         $('small[data-validator=firstName]').removeClass('d-block').addClass("d-none");
         $('small[data-validator=lastName]').removeClass('d-block').addClass("d-none");
         $('small[data-validator=confirm-password]').removeClass('d-block').addClass("d-none");
         $('small[data-validator=password]').removeClass('d-block').addClass("d-none");
         $('small[data-validator=email]').removeClass('d-block').addClass("d-none");
-        
-        if ($("input[name=firstname]").val() === "") {
+        $('.toast').toast('show');
+        if ($("input[name=firstName]").val() === "") {
             $('small[data-validator=firstName]').addClass("d-block");
         }
         if ($("input[name=lastName]").val() === "") {
@@ -19,7 +22,7 @@ $(function () {
         if ($("input[name=password]").val() === "") {
             $('small[data-validator=password]').addClass("d-block");
         }
-        if ($("input[name=email]").val() === "") {
+        if ($("input[name=email]").val() === "" || ($("input[name=email]").val().length && !regex.test($("input[name=email]").val()))) {
             $('small[data-validator=email]').addClass("d-block");
         }
      }
