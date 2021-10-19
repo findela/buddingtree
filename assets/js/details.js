0735f2f44bbf8334d36cc2b7216a3e46210f2126
Tamaghna Banerjee <tamaghna.ba@imerit.net>
2021-10-19 14:46:49 +0530

    Updated changes in buddingTree project

--- assets/js/details.js
+++ assets/js/details.js
@@ -1,10 +1,10 @@
 $(function() {
-
   //selector to find selectable element
   $(".image-overlay-thumbnail").click(function() {
     const selector = $(this).find("img");
     $("#image-container").attr("src", selector.attr("src"));
     $("#lightbox-overlay").find("img").attr("src", selector.attr("src"));
+
     $(".image-overlay-thumbnail").find("img").removeClass("border-2x border-theme-primary").addClass("border");
     selector.addClass("border-2x border-theme-primary").removeClass("border");
   });
@@ -13,6 +13,6 @@ $(function() {
   $("a.review-btn").click(function() {
     $([document.documentElement]).animate({
       scrollTop: $("#customer-reviews").offset().top
-    }, 1200);
+    }, 2000);
   });
 });
